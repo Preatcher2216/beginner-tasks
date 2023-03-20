19a866000fa66479ee11b47a225d2bfa0e795626
i.veshkin <i.veshkin@inostudio.com>
2023-03-20 13:13:48 +0300

    Init commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    trailingComma: 'all',
+    tabWidth: 2,
+    semi: true,
+    singleQuote: true,
+    printWidth: 120,
+    bracketSpacing: false,
+    arrowParens: 'always',
+    endOfLine: 'auto',
+};
